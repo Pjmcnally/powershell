44f43024a7fafd7e37e9431d0f3fc92af403db5a
Patrick McNally <Patrick@pjmcnally.net>
2017-07-29 13:10:37 -0500

    Add function to rename files

--- dnd_file_re-org.ps1
+++ dnd_file_re-org.ps1
@@ -1,15 +1,32 @@
-$files = Get-ChildItem "C:\Users\Pjmcnally\Downloads\Generic Pregen Chars\By Character" -Recurse -Filter *.pdf
+$path = "C:\Users\Pjmcnally\Downloads\Generic Pregen Chars\By Character" 
+$files = Get-ChildItem $path -Recurse -Filter *.pdf
 
 # for ($i=1; $i -lt 11; $i++) {
 #     $reg = '^.*' + $i + '.pdf$'
 #     $reg
+#     Foreach($file in $files) {
+#         $name = $file.Fullname
+#         if ($name -match $reg) {
+#             $name
+#         }
+#     }
 # }
 
 
-Foreach($file in $files) {
-	$name = $file.Fullname
-    if ($name -match '^.*1.pdf$') {
-        $name
-    }
+# Rename file to add leading 0
+# Foreach($file in $files) {
+#         $name = $file.Fullname
+#         if ($name -match $reg) {
+#             $name
+#         }
+#     }
+
+function rename($file) {
+    $name = $file.Fullname
+    $name -replace '^(?<beg>[^\d]*)(?<num>\d{1}).pdf', '${beg}0${num}.pdf'
 }
 
+
+Foreach($file in $files) {
+    rename($file)
+}
