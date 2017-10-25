3982b0f1e1ffef264616af722cf92c2fc18a3d88
Patrick McNally <patrick.j.mcnally@gmail.com>
2017-10-25 15:14:54 -0500

    First commit sort_pdf

---
+++ pdf/sort_pdf.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    This script sorts PDFs by some value in the name (in this case serial number) into 
+    folders based on that number.
+#>
+
+cd E:\PayPal-work\SinglePdf
+$dest = 'E:\new-files'
+
+$a = Get-ChildItem
+ForEach ($file in $a) {
+    $asn = $file.name -replace '^\d+-(?<asn>\d+).+\.pdf$', '${asn}'
+    $to = Join-Path $dest $asn
+    
+    if (!(Test-Path $to)){
+        New-Item -Path $to -ItemType Directory -Force | Out-Null
+    }
+
+    Move-Item -Path $file.FullName -Destination $to
+}
