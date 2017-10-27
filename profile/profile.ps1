d5bf7d11223691a3a00e936d1fdad8152ccf8d82
Patrick McNally <patrick.j.mcnally@gmail.com>
2017-10-27 09:57:52 -0500

    Change workon to use [enum]env for validateSet and change location of import

--- profile/profile.ps1
+++ profile/profile.ps1
@@ -6,6 +6,9 @@ Import-Module posh-git
 $GitPromptSettings.DefaultPromptPrefix = '`n'
 $GitPromptSettings.DefaultPromptSuffix = '`n$(''>'' * ($nestedPromptLevel + 1)) '
 
+# Import systemVariables file ([enum]envs, [hashtable]env_dict)
+. $HOME\Documents\WindowsPowerShell\systemVariables.ps1
+
 # My functions
 function workon {
 <#
@@ -17,24 +20,24 @@ function workon {
             Mandatory=$True,
             Position=0,
             HelpMessage="Enter the project name or 'help' for list of projects.")]
-        [ValidateSet('help', 'ahk', 'pshell', 'devops', 'iptools')]
-        $project
+        [envs]$env_name
     )
 
     # To keep this modular $dict is now linked to an external file I can modify
     # on a system by system basis while keep the rest of this func the same.
-    . $HOME\Documents\WindowsPowerShell\systemVariables.ps1
-    $dict = $env_dict
+
+    # Convert param to string to use as dict key
+    $project = ($env_name.ToString())
 
     if ($project -eq 'help') {  # Display help text
         Write-Host ("{0,-15}{1}" -f "`nProject Name", "Code run")
         Write-Host ("{0,-15}{1}" -f "============", "========")
-        $dict.GetEnumerator() | Sort-Object Name | ForEach-Object {
+        $env_dict.GetEnumerator() | Sort-Object Name | ForEach-Object {
             Write-Host ("{0,-15}{1}" -f $_.key, $_.value)
         }
     } else {  # Run command associated with key
         Try {
-            invoke-expression ($dict.$project) -ErrorAction stop
+            invoke-expression ($env_dict.$project) -ErrorAction stop
             return
         } Catch {  # Error out on invalid command
             Write-Host "Not a valid command for workon"
