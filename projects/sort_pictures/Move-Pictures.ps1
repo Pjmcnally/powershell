61ecdc35b15e912f7bd830ff7c74dfdb13e0217a
Patrick McNally <Patrick.J.McNally@gmail.com>
2018-07-10 00:15:26 -0500

    Update script to also process by date in filename.

--- projects/sort_pictures/Move-Pictures.ps1
+++ projects/sort_pictures/Move-Pictures.ps1
@@ -1,4 +1,4 @@
-﻿function Get-DateTimeFormat () {
+function Get-ShortDateTimeFormat () {
     # Setup DateTime format
     # Extract the default Date/Time formatting from the local computer's "Culture" settings, and then create the format to use when parsing the date/time information pull from AD.
     $CultureDateTimeFormat = (Get-Culture).DateTimeFormat
@@ -16,34 +16,56 @@ function Move-Pictures () {
         [string]$sourceFoldPath,
 
         [Parameter(Mandatory=$True, Position=1)]
-        [string]$destFoldPath,
+        [string]$destFoldPath
     )
 
     # Setup shell objects
     $objShell  = New-Object -ComObject Shell.Application
     $objFolder = $objShell.namespace($sourceFoldPath)
 
-    # Get DateTimeFormat for interpreting dates
-    $datetimeFormat = Get-DateTimeFormat
+    # Get DateTimeFormat for interpreting system dates
+    $shortDatetimeFormat = Get-ShortDateTimeFormat
+
+    # Set dateformat for filename dates
+    $filenameDateFormat = 'yyyy-MM-dd'
 
     # Iterate over files
     ForEach($file in $objFolder.Items()) {
+        # Reset date to null
+        $date = $null
+
         # This is a magic number. 12 is the int value of the "date taken" property.
         $dateTaken = $objFolder.getDetailsOf($File, 12)
 
-        if (!$dateTaken) {
-            Continue
+
+        if ($dateTaken) {
+            # I do not know why but there are stupid characters in these dates. Remove them.
+            $cleanDate = $dateTaken -Replace "(\u200F|\u200E)", ""
+
+            # Convert date to datetime
+            $date = [DateTime]::ParseExact(
+                $CleanDate,
+                $DateTimeFormat,
+                [System.Globalization.DateTimeFormatInfo]::InvariantInfo
+            )
+        } else {
+            $fileName = $objFolder.getDetailsOf($File, 0)
+            $m = $fileName -Match "(?<date>\d{4}-\d{2}-\d{2})"
+            if ($matches) {
+                $strDate = $matches.Date
+
+                # Convert date to datetime
+                $date = [DateTime]::ParseExact(
+                    $strDate,
+                    $filenameDateFormat,
+                    [System.Globalization.DateTimeFormatInfo]::InvariantInfo
+                )
+            }
         }
 
-        # I do not know why but there are stupid characters in these dates. Remove them.
-        $dateClean = $dateTaken -Replace "(\u200F|\u200E)", ""
-
-        # Convert date to datetime
-        $date = [DateTime]::ParseExact(
-            $dateClean,
-            $DateTimeFormat,
-            [System.Globalization.DateTimeFormatInfo]::InvariantInfo
-        )
+        if (!$date) {
+            continue
+        }
 
         $newFoldPath = Join-Path $destFoldPath ("{0}_{1:00}" -f ($date.Year, $date.Month))
 
