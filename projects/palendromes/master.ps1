d18c73d54a2c1b0c47269433888c913cb59a4a9f
Patrick McNally <Patrick.J.McNally@gmail.com>
2018-07-16 22:12:08 -0500

    Rename files.

--- projects/palendromes/master.ps1
+++ projects/palendromes/master.ps1
@@ -1,6 +1,6 @@
 Write-Host "Running Python file:"
-Write-Host "Results = $(python temp.py)"
-python -m timeit -u sec -r 5 -s "import temp" -p "temp.main()"
+Write-Host "Results = $(python palendrome.py)"
+python -m timeit -u sec -r 5 -s "import palendrome" -p "palendrome.main()"
 
 Write-Host "`r`nRunning PowerShell file:"
-Write-Host "$(Measure-Command {Write-Host Results = $(./temp.ps1)})"
+Write-Host "$(Measure-Command {Write-Host Results = $(./palendrome.ps1)})"
