134e9f38b7363e682e3ee6579c0fa6c360135374
Patrick McNally <Patrick.J.McNally@gmail.com>
2018-08-15 11:07:08 -0500

    Add new file.

---
+++ projects/folder-check/temp_script.ps1
@@ -0,0 +1,18 @@
+﻿$folder = "C:\Users\Patrick\Desktop\test"
+$file = "C:\Users\Patrick\Desktop\temp.txt"
+
+While ($true) {
+    $date = Get-Date
+    if($date.Second % 10 -eq 0) {
+        $num = (Get-ChildItem $folder).Count
+        
+        $str = "$num files remaining in folder at $($date.ToString('HH:mm:ss'))"
+
+        Write-Host $str
+        $str >> $file
+
+        Start-Sleep -Seconds 8
+    } else {
+        Start-Sleep -Millisecond 50
+    }
+}
