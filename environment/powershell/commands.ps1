a212bff6e2c3d04b957b7a58227aac0bbc8b3561
Patrick McNally <Patrick.J.McNally@gmail.com>
2021-04-06 14:16:14 -0500

    Git add commands.ps1

---
+++ environment/powershell/commands.ps1
@@ -0,0 +1,9 @@
+# Commands used to install or setup new environment
+
+# Install chocolatey - Instructions found here: https://chocolatey.org/install
+
+# Install OpenSSH using current version (Not one packaged with windows)
+choco install openssh -params "/SSHServerFeature /SSHAgentFeature"
+
+# Install FiraCode font
+choco install FiraCode
